a3e6ef1bba6bbd07854c00be2d6ce2fe54dcea41
melizasosa <s.m.meliza@gmail.com>
2019-06-07 06:56:00 -0500

    cifrado terminado

--- src/cipher.js
+++ src/cipher.js
@@ -5,8 +5,42 @@ window.cipher = {
 
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
-       let string=textoIngresadoCifrar.value;
-       console.log("hola");
+       let textoSalidaCifrar='';
+       /*
+       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+       let textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                    textoSalidaCifrar += String.fromCharCode(textChar);*/
+                    console.log(string);
+                    console.log(offset);
+                    console.log(string.length);
+                    for (let i = 0; i < string.length; i++) {
+                                let character = string[i];
+                                console.log(character)
+                                if (character.match(/[a-z]/i)) {
+                                  console.log("estramos al primer if")
+                                  console.log(string.charCodeAt(i))
+
+                                    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                                        let textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                                        textoSalidaCifrar += String.fromCharCode(textChar);
+                                        console.log(textoSalidaCifrar)
+                                        console.log("estramos al segundo if if")
+                                    } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                                        textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                                        textoSalidaCifrar += String.fromCharCode(textChar);
+                                        console.log(textoSalidaCifrar)
+                                        console.log("estramos al segundo if if")
+
+                                    }
+                                } else {
+                                    textoSalidaCifrar += character;
+                                }
+                            }
+                            return textoSalidaCifrar;
+/*
+        let textoCifrar=string.charCodeAt(0);
+        let textChar = (string.charCodeAt(0) - 65 + parseInt(offset)) % 26 + 65;
+                     textoSalidaCifrar += String.fromCharCode(textChar);*/
 
          },
   decode: (offset, string) => {
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const msj_bloqueado=document.getElementById("bloqueo");
 const msj_error=document.getElementById("error");
 //const btn_ingresar=document.getElementById("ingresar");
 const textoIngresadoCifrar=document.getElementById("texto_input");
-const numDes=document.getElementById("numdDesplazar");
+const numDes=document.getElementById("number");
+const textoSalidaCifrar=document.getElementById("text_out");
 
 let cont=0;
 
@@ -23,7 +24,7 @@ const boton_principal=document.getElementById("principal")
 const vistaD=document.getElementById("vistaDos");
 
 //Funcion para la validacion de la clave
-enlace.addEventListener ('click',()=>{
+/*enlace.addEventListener ('click',()=>{
   const clave=password.value;
 
   if(clave==="LABORATORIA"){
@@ -44,14 +45,43 @@ enlace.addEventListener ('click',()=>{
     //smj_bloqueado.classList.add('show')
     console.log(cont);
   }
-});
+});*/
+
+function clave(){
+  const clave=password.value;
+
+  if(clave==="LABORATORIA"){
+      window.location.href="index2.html"
+      cont=0;
+
+    }else if(cont>=2){
+     msj_bloqueado.innerHTML='Intento varias veces'
+     sms_error.classList.add("hide")
+     btn_ingresar.classList.add("hide")
+     console.log(cont);
+   }
+   else{
+    cont=cont+1;
+     msj_error.innerHTML='Clave incorrecta'
+    // msj_bloqueado.innerHTML='Intento varias veces'
+     //sms_error.classList.add("hide")
+    //smj_bloqueado.classList.add('show')
+    console.log(cont);
+  }
+}
 
 //funcion de limpiar
-limpiar.addEventListener('click',()=>{
+/*limpiar.addEventListener('click',()=>{
       document.getElementById("password").value="";
       //sms_error.classList.add("hide")
      //sms.style.display="none";
-});
+});*/
+
+function limpiarClave(){
+  document.getElementById("password").value="";
+}
+
+
 
 //funcion SALIR
 /*btn_salir.addEventListener('click',()=>{
@@ -76,11 +106,27 @@ function btn_clean(){
 
 //funcion cifrar
 function btn_cifrar(){
+  /*let string;
+  let text_out;
   let textoCifrar=textoIngresadoCifrar.value;
   let numDesplazamiento=numDes.value;
-  document.getElementById("text_out").innerHTML=window.cipher.encode(text_out,string);
+
+
+  console.log(textoCifrar);
+  console.log(numDesplazamiento);
+  textoSalidaCifrar.value=textoCifrar;
+  console.log(textoSalidaCifrar);
+  textoCifrar.innerHTML=window.cipher.encode(offset,string);*/
+
+  //let string;
+  let textoCifrar;
+
+
+  let string=textoIngresadoCifrar.value;
+  let offset=numDes.value;
+
+  textoSalidaCifrar.value=string;
+  console.log(textoSalidaCifrar);
+  textoSalidaCifrar.value=window.cipher.encode(offset,string);
+
 }
-
-btn_cifrar.addEventListener('click',()=>{
-
-})
