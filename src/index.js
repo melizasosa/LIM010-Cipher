dba24fb39a5a49f30c57954a5b8c2a395c5aae82
melizasosa <s.m.meliza@gmail.com>
2019-06-14 06:45:53 -0500

    resolviendo merge

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 /* Acá va tu código */
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-//VARIABLES
-const primeravista=document.getElementById("primeravista");
-const segundavista=document.getElementById("segundavista");
-const msj_error=document.getElementById("msj_error");
-const password=document.getElementById("password");
-const msj_bloqueado=document.getElementById("sms_bloqueo");
-let textoIngresado=document.getElementById("texto_ingresado");
-const numDes=document.getElementById("number");
-const textoSalidaCifrar=document.getElementById("text_out");
->>>>>>> 2c0bcabb513751fc0e95a4107e2595c77c0029ba
-
-
-<<<<<<< HEAD
-=======
 //VARIABLES
 const primeravista=document.getElementById("primeravista");
 const segundavista=document.getElementById("segundavista");
@@ -30,12 +13,6 @@ const segvista=document.getElementById("segvista");
 const textoClave=document.getElementById("texto_clave");
 
 //VARIABLES PARA EL CIFRADO
-
-//VARIABLE CONTADOR
->>>>>>> 7d1b4ee... -nuevos cambios
-let cont=0;
-=======
->>>>>>> 2c0bcabb513751fc0e95a4107e2595c77c0029ba
 
 //VARIABLE CONTADOR
 let cont=0;
@@ -53,25 +30,6 @@ const btn_limpiar=document.getElementById("button_limpiar");
 btn_ingresar.addEventListener ("click",(event)=>{
   event.preventDefault();
   const clave=password.value;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-  console.log(clave);
->>>>>>> 2c0bcabb513751fc0e95a4107e2595c77c0029ba
-  if(clave==="LABORATORIA"){
-    //  window.location.href="index2.html"
-      segundavista.classList.add("show");
-      primeravista.classList.add("hide");
-      cont=0;
-
-    }else if(cont>=2){
-<<<<<<< HEAD
-     msj_bloqueado.innerHTML='Intento varias veces'
-     sms_error.classList.add("hide")
-     btn_ingresar.classList.add("hide")
-     console.log(cont);
-=======
   if(clave==="LABORATORIA"){
     //  window.location.href="index2.html"
       segundavista.classList.add("show");
@@ -84,14 +42,6 @@ btn_ingresar.addEventListener ("click",(event)=>{
        password.classList.add("hide")
        textoClave.classList.add("hide")
        msj_bloqueado.innerHTML=":( LO SENTIMOS,NO SE PUEDO INICIAR SESIÓN"
->>>>>>> 7d1b4ee... -nuevos cambios
-=======
-       msj_error.classList.add("hide");
-       btn_ingresar.classList.add("hide")
-       password.classList.add("hide")
-       msj_bloqueado.innerHTML="SUPERÓ EL LIMITE PERMITIDO"
-
->>>>>>> 2c0bcabb513751fc0e95a4107e2595c77c0029ba
    }
    else{
        cont=cont+1;
@@ -100,14 +50,6 @@ btn_ingresar.addEventListener ("click",(event)=>{
     }
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-//funcion de limpiar
-limpiar.addEventListener('click',()=>{
-      document.getElementById("password").value="";
-      //sms_error.classList.add("hide")
-     //sms.style.display="none";
-=======
 
 //funcion SALIR
 btn_salir.addEventListener('click',(event)=>{
@@ -117,16 +59,6 @@ btn_salir.addEventListener('click',(event)=>{
   segundavista.classList.add("hide");
   primeravista.classList.add("show");
   login-container.classList.add("show");
->>>>>>> 7d1b4ee... -nuevos cambios
-});
-=======
->>>>>>> 2c0bcabb513751fc0e95a4107e2595c77c0029ba
-
-//funcion SALIR
-btn_salir.addEventListener('click',(event)=>{
-  event.preventDefault();
-    primeravista.classList.add("show");
-    segundavista.classList.add("hide");
 });
 
 //funcion limpiar texto
@@ -159,4 +91,4 @@ btn_desCifrar.addEventListener('click',(event)=>{
   let offset=numDes.value;
   textoSalidaCifrar.value=window.cipher.decode(offset,string);
 
-});
+})
