aafedffe1dad9637e80f79de859546dfcd013976
melizasosa <s.m.meliza@gmail.com>
2019-06-14 06:23:33 -0500

    proyecto terminado

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-# Cifrado César
+#SISTEMA DE ENCRIPTACION POLICIAL
 
-## Usuarios
+Es un sistema mediante el cual los usuarios que son los comandantes y jefes policiales podrán transmitir sus mensajes con seguridad de que no podrá ser filtrado; en caso de que sea filtrado no comprenderán el mensaje real debido a que serán frases sin sentido para el que carezca de información completa y del sistema de encriptación.
+
+- PROTOTIPO FINAL
+![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/LoginSistema.jng "Prototipo Sistema vista login")
+
+![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoSistema.jng "Prototipo Sistema vista cifrado")
+
+##INVESTIGACION IX
+### Usuarios
 
 El producto está dirigido para los comandantes y jefes de operativos policiales del Perú.
 
-## Objetivos
+### Objetivos
 
-Está dirigido para realizar operativos policiales  protegiendo información confidencial mediante el uso del algoritmo  cifrado César.  
-
-Es un sistema mediante el cual los usuarios podrán transmitir sus mensajes con seguridad de que no podrá ser filtrado; en caso de que sea filtrado no comprenderán el mensaje real debido a que serán frases sin sentido para el que carezca de información completa y del sistema de encriptación.
+Dicho sistema servira para operativos policiales  protegiendo información confidencial mediante el uso del algoritmo  cifrado César.  
 
 Actualmente los centros policiales tienen un sistema estadounidense de encriptación pero las zonas de emergencia como el VRAE debido al alto costo  y no es accesible para todo los centros policiales del Perú.
+<<<<<<< HEAD
+=======
+
+###Primer Prototipo en lapiz y papel
+![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoInicial_vista1.jpg "Prototipo inicial vista 1")
+Mejorar el diseño del login
+
+![Prototipo inicial vista 2](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoInicial_vista2.jpg "Prototipo inicial vista 2")
+Agregar un boton limpiar, salir y que sea en un formulario al centro.
+
+###Prototipo final en Balsamiq
+![Prototipo final vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoFinal_vista1.jpg "Prototipo final vista 1")
+
+![Prototipo final vista 2](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoFinal_vista2.PNG "Prototipo final vista 2")
+>>>>>>> 7d1b4ee... -nuevos cambios
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* Acá va tu código */
+<<<<<<< HEAD
 
 //Variables
 const password=document.getElementById('password');
@@ -10,6 +11,24 @@ const msj_error=document.getElementById("error");
 const textoIngresadoCifrar=document.getElementById("texto_input");
 const numDes=document.getElementById("numdDesplazar");
 
+=======
+//VARIABLES
+const primeravista=document.getElementById("primeravista");
+const segundavista=document.getElementById("segundavista");
+const msj_error=document.getElementById("msj_error");
+const password=document.getElementById("password");
+const msj_bloqueado=document.getElementById("sms_bloqueo");
+let textoIngresado=document.getElementById("texto_ingresado");
+const numDes=document.getElementById("number");
+const textoSalidaCifrar=document.getElementById("text_out");
+const cifrado=document.getElementById("cifrado");
+const segvista=document.getElementById("segvista");
+const textoClave=document.getElementById("texto_clave");
+
+//VARIABLES PARA EL CIFRADO
+
+//VARIABLE CONTADOR
+>>>>>>> 7d1b4ee... -nuevos cambios
 let cont=0;
 
 
@@ -25,6 +44,7 @@ const vistaD=document.getElementById("vistaDos");
 //Funcion para la validacion de la clave
 enlace.addEventListener ('click',()=>{
   const clave=password.value;
+<<<<<<< HEAD
 
   if(clave==="LABORATORIA"){
       window.location.href="index2.html"
@@ -35,6 +55,20 @@ enlace.addEventListener ('click',()=>{
      sms_error.classList.add("hide")
      btn_ingresar.classList.add("hide")
      console.log(cont);
+=======
+  if(clave==="LABORATORIA"){
+    //  window.location.href="index2.html"
+      segundavista.classList.add("show");
+      primeravista.classList.add("hide");
+      cont=0;
+
+    }else if(cont>=2){
+       msj_error.classList.add("hide");
+       btn_ingresar.classList.add("hide")
+       password.classList.add("hide")
+       textoClave.classList.add("hide")
+       msj_bloqueado.innerHTML=":( LO SENTIMOS,NO SE PUEDO INICIAR SESIÓN"
+>>>>>>> 7d1b4ee... -nuevos cambios
    }
    else{
     cont=cont+1;
@@ -46,11 +80,23 @@ enlace.addEventListener ('click',()=>{
   }
 });
 
+<<<<<<< HEAD
 //funcion de limpiar
 limpiar.addEventListener('click',()=>{
       document.getElementById("password").value="";
       //sms_error.classList.add("hide")
      //sms.style.display="none";
+=======
+
+//funcion SALIR
+btn_salir.addEventListener('click',(event)=>{
+  event.preventDefault();
+  cifrado.classList.add("hide");
+  segvista.classList.add("hide");
+  segundavista.classList.add("hide");
+  primeravista.classList.add("show");
+  login-container.classList.add("show");
+>>>>>>> 7d1b4ee... -nuevos cambios
 });
 
 //funcion SALIR
