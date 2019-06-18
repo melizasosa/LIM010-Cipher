49863bcdabcc2b27cd908db3097bdd3c9adf117c
melizasosa <s.m.meliza@gmail.com>
2019-06-18 11:03:49 -0500

    Bloqueo de la caja de resultado

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
-#SISTEMA DE ENCRIPTACION POLICIAL
+# SISTEMA DE ENCRIPTACION POLICIAL
 
 Es un sistema mediante el cual los usuarios que son los comandantes y jefes policiales podrán transmitir sus mensajes con seguridad de que no podrá ser filtrado; en caso de que sea filtrado no comprenderán el mensaje real debido a que serán frases sin sentido para el que carezca de información completa y del sistema de encriptación.
 
 - PROTOTIPO FINAL
-
 ![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/LoginSistema.PNG)
 
 ![Prototipo inicial vista 2](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoSistema.PNG)
 
 ## INVESTIGACION IX
-![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/LoginSistema.jng "Prototipo Sistema vista login")
-
-![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoSistema.jng "Prototipo Sistema vista cifrado")
-
-##INVESTIGACION IX
-
 ### Usuarios
 
 El producto está dirigido para los comandantes y jefes de operativos policiales del Perú.
@@ -25,10 +18,7 @@ Dicho sistema servira para operativos policiales  protegiendo información confi
 
 Actualmente los centros policiales tienen un sistema estadounidense de encriptación pero las zonas de emergencia como el VRAE debido al alto costo  y no es accesible para todo los centros policiales del Perú.
 
-
 ### Primer Prototipo en lapiz y papel
-
-
 ![Prototipo inicial vista 1](https://github.com/melizasosa/LIM010-Cipher/blob/master/src/img/PrototipoInicial_vista1.jpg "Prototipo inicial vista 1")
 Mejorar el diseño del login
 
--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,34 @@
 
 window.cipher = {
 
-
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
-       let string=textoIngresadoCifrar.value;
-       console.log("hola");
+    let textoSalidaCifrar='';
+    let textChar;
 
-         },
-
-window.cipher = {
-
-  encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
-       let textoSalidaCifrar='';
-       let textChar;
-                    for (let i = 0; i < string.length; i++) {
-                              let character = string[i];
-                              //condicion para letras mayusculas
-                              if (character.match(/[a-z]/i)) {
-
-                                    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-                                        textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-                                        textoSalidaCifrar += String.fromCharCode(textChar);
-
-                                    }
-                                        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-                                        textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) %26 + 97;
-                                        textoSalidaCifrar += String.fromCharCode(textChar);
-
-                                    }
-                                } else {
-                                  textoSalidaCifrar += character;
-                                }
-                            }
-                            return textoSalidaCifrar;
-         },
-
+    for (let i = 0; i < string.length; i++) {
+      console.log(string+"String");
+      let character = string[i];
+      console.log(character+"CARACTER");
+      console.log(string+"2STRING");
+      //condicion para letras mayusculas
+      if (character.match(/[a-z]/i)) {
+        console.log(character);
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+          textoSalidaCifrar += String.fromCharCode(textChar);
+        }
+          else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+            textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) %26 + 97;
+            textoSalidaCifrar += String.fromCharCode(textChar);
+          }
+        }
+        else {
+          textoSalidaCifrar += character;
+        }
+      }
+    return textoSalidaCifrar;
+  },
 
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
@@ -44,23 +36,23 @@ window.cipher = {
      let textChar;
 
      for (let i = 0; i < string.length; i++) {
-              let character = string[i];
-              if (character.match(/[a-z]/i)) {
-                    //condicion para descifrar letras  mayuscula
-                    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-                        let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
-                        textoSalidaCifrar += String.fromCharCode(textChar);
-                        }
-                        //condicion para descifrar para letras minusculas
-                        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-                        textChar = (string.charCodeAt(i) - 97 - parseInt(offset)+52) % 26 +97;
-                        textoSalidaCifrar += String.fromCharCode(textChar);
-                    }
-                }
-                else {
-                  textoSalidaCifrar += character;
-                }
-            }
-            return textoSalidaCifrar;
+       let character = string[i];
+       if (character.match(/[a-z]/i)) {
+       //condicion para descifrar letras  mayuscula
+         if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+           let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+           textoSalidaCifrar += String.fromCharCode(textChar);
+          }
+          //condicion para descifrar para letras minusculas
+         else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+           textChar = (string.charCodeAt(i) - 97 - parseInt(offset)+52) % 26 +97;
+           textoSalidaCifrar += String.fromCharCode(textChar);
+          }
+        }
+        else {
+          textoSalidaCifrar += character;
+        }
+    }
+    return textoSalidaCifrar;
   }
 };
