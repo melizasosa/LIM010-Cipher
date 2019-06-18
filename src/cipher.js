82f9df53419ae91de8eafd69c1cc987d6f6efb41
melizasosa <s.m.meliza@gmail.com>
2019-06-17 21:31:46 -0500

    mejora del formulario

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,55 @@
 
-
 window.cipher = {
-
 
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
-       let string=textoIngresadoCifrar.value;
-       console.log("hola");
+       let textoSalidaCifrar='';
+       let textChar;
+                    for (let i = 0; i < string.length; i++) {
+                              let character = string[i];
+                              //condicion para letras mayusculas
+                              if (character.match(/[a-z]/i)) {
 
+                                    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                                        textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                                        textoSalidaCifrar += String.fromCharCode(textChar);
+
+                                    }
+                                        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                                        textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) %26 + 97;
+                                        textoSalidaCifrar += String.fromCharCode(textChar);
+
+                                    }
+                                } else {
+                                  textoSalidaCifrar += character;
+                                }
+                            }
+                            return textoSalidaCifrar;
          },
+
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
+     let textoSalidaCifrar='';
+     let textChar;
+
+     for (let i = 0; i < string.length; i++) {
+              let character = string[i];
+              if (character.match(/[a-z]/i)) {
+                    //condicion para descifrar letras  mayuscula
+                    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                        let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+                        textoSalidaCifrar += String.fromCharCode(textChar);
+                        }
+                        //condicion para descifrar para letras minusculas
+                        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                        textChar = (string.charCodeAt(i) - 97 - parseInt(offset)+52) % 26 +97;
+                        textoSalidaCifrar += String.fromCharCode(textChar);
+                    }
+                }
+                else {
+                  textoSalidaCifrar += character;
+                }
+            }
+            return textoSalidaCifrar;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,86 +1,95 @@
 /* Acá va tu código */
+//VARIABLES
+const primeravista=document.getElementById("primeravista");
+const segundavista=document.getElementById("segundavista");
+const msj_error=document.getElementById("msj_error");
+const password=document.getElementById("password");
+const msj_bloqueado=document.getElementById("sms_bloqueo");
+let textoIngresado=document.getElementById("texto_ingresado");
+const numDes=document.getElementById("number");
+const textoSalidaCifrar=document.getElementById("text_out");
+const cifrado=document.getElementById("cifrado");
+const segvista=document.getElementById("segvista");
+const textoClave=document.getElementById("texto_clave");
 
-//Variables
-const password=document.getElementById('password');
-const sms=document.getElementById("sms_error");
-const inicio=document.getElementById("Login");
-const msj_bloqueado=document.getElementById("bloqueo");
-const msj_error=document.getElementById("error");
-//const btn_ingresar=document.getElementById("ingresar");
-const textoIngresadoCifrar=document.getElementById("texto_input");
-const numDes=document.getElementById("numdDesplazar");
+//VARIABLES PARA EL CIFRADO
 
+//VARIABLE CONTADOR
 let cont=0;
 
 
+//VARIABLES PARA LOS BOTONES
+const btn_ingresar=document.getElementById("button_ingresar");
+const btn_salir=document.getElementById("button_salir");
+const btn_cifrar=document.getElementById("button_cifrar");
+const btn_desCifrar=document.getElementById("button_desCifrar");
+const btn_limpiar=document.getElementById("button_limpiar");
 
-//variables de los botones
-const enlace=document.getElementById("ingresar");
-const limpiar=document.getElementById('ingresar');
-const btn_salir=document.getElementById("btn_out");
-const boton_principal=document.getElementById("principal")
 
-const vistaD=document.getElementById("vistaDos");
-
-//Funcion para la validacion de la clave
-enlace.addEventListener ('click',()=>{
+//FUNCION PARA VALIDAR CLAVE
+btn_ingresar.addEventListener ("click",(event)=>{
+  event.preventDefault();
   const clave=password.value;
-
   if(clave==="LABORATORIA"){
-      window.location.href="index2.html"
+    //  window.location.href="index2.html"
+      segundavista.classList.add("show");
+      primeravista.classList.add("hide");
       cont=0;
 
     }else if(cont>=2){
-     msj_bloqueado.innerHTML='Intento varias veces'
-     sms_error.classList.add("hide")
-     btn_ingresar.classList.add("hide")
-     console.log(cont);
+       msj_error.classList.add("hide");
+       btn_ingresar.classList.add("hide")
+       password.classList.add("hide")
+       textoClave.classList.add("hide")
+       msj_bloqueado.innerHTML=":( LO SENTIMOS,NO SE PUEDO INICIAR SESIÓN"
    }
    else{
-    cont=cont+1;
-     msj_error.innerHTML='Clave incorrecta'
-    // msj_bloqueado.innerHTML='Intento varias veces'
-     //sms_error.classList.add("hide")
-    //smj_bloqueado.classList.add('show')
-    console.log(cont);
-  }
+       cont=cont+1;
+       msj_error.innerHTML='Clave incorrecta'
+       document.getElementById("password").value="";
+    }
 });
 
-//funcion de limpiar
-limpiar.addEventListener('click',()=>{
-      document.getElementById("password").value="";
-      //sms_error.classList.add("hide")
-     //sms.style.display="none";
-});
 
 //funcion SALIR
-/*btn_salir.addEventListener('click',()=>{
-  //console.log("hola");
-    alert("estamos dentro de la Funcion")
+btn_salir.addEventListener('click',()=>{
 
-    inicio.classList.add("show")
-    boton_principal.classList.add("show")
-    window.location.href="index.html"
-})*/
+  cifrado.classList.add("hide");
+  segvista.classList.add("hide");
+  segundavista.classList.add("hide");
+  primeravista.classList.add("show-flex");
+  document.getElementById("password").value="";
 
-//funcion salir sesion
-function salir1(){
-  console.log("hola");
-  window.location.href="index.html"
-}
+  //login-container.classList.add("show");
+});
 
 //funcion limpiar texto
-function btn_clean(){
-  document.getElementById("texto_input").value="";
-}
+btn_limpiar.addEventListener('click',()=>{
 
-//funcion cifrar
-function btn_cifrar(){
-  let textoCifrar=textoIngresadoCifrar.value;
-  let numDesplazamiento=numDes.value;
-  document.getElementById("text_out").innerHTML=window.cipher.encode(text_out,string);
-}
+  textoIngresado.value="";
+  numDes.value="";
+  textoSalidaCifrar.value="";
 
-btn_cifrar.addEventListener('click',()=>{
+  segundavista.classList.add("show");
+  primeravista.classList.add("hide");
+});
+
+
+btn_cifrar.addEventListener("click",()=>{
+
+  //Valores de los datos ingresados
+  let string=textoIngresado.value;
+  let offset=numDes.value;
+
+  textoSalidaCifrar.value=window.cipher.encode(offset,string);
+
+});
+
+//FUNCION PARA DESCIFRAR
+btn_desCifrar.addEventListener('click',()=>{
+  //Valores de los datos ingresados
+  let string=textoIngresado.value;
+  let offset=numDes.value;
+  textoSalidaCifrar.value=window.cipher.decode(offset,string);
 
 })
